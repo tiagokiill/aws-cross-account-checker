9085cda3ad5d8c76d8a86aac210c32e7803ed18f
teopanella <teo.panella@gmail.com>
2022-04-12 05:55:21 -0300

    add get orgs details

--- checker/main.py
+++ checker/main.py
@@ -8,58 +8,66 @@ import boto3
 import json
 
 
-def get_roles():
+def get_roles(session):
     """
         Explanation: Method defined to get all rules from AWS
-        :params: No params required
-        :return: list os AWS accounts from rules
+        :params: str session
+        :return: dict of os AWS accounts from rules found
     """
 
-    client = boto3.client('iam')
+    client = boto3.client('iam',
+                        aws_access_key_id=session['Credentials']['AccessKeyId'],
+                        aws_secret_access_key=session['Credentials']['SecretAccessKey'],
+                        aws_session_token=session['Credentials']['SessionToken'])
+
     response = client.list_roles()
-    list = []
+    list_of_roles = []
+
     for a in response['Roles']:
-        path = a['Path']
-        role_name = a['RoleName']
-        role_id = a['RoleId']
-        arn = a['Arn']
 
         for b in a['AssumeRolePolicyDocument']['Statement']:
             effect = b['Effect']
             external_arn = b['Principal'].get('AWS', 'Access From AWS Native Service')
-            external_action = b['Action']
+            #external_action = b['Action']
 
             if external_arn != 'Access From AWS Native Service':
-                list.append(external_arn[13:25])
-                #print(
-                #    f'**ALERT** External account ID: {external_arn[13:25]}, '
-                #    f'Effect of role: {effect}, '
-                #    f'Action of role: {external_action}')
+                list_of_roles.append(a)
 
-    return list
+#                dic_item[a['RoleId']] = a['RoleName']
+#                dic_item['external_account_id'] = external_arn[13:25]
+#                dic_item['role_arn'] = a['Arn']
 
 
-def get_orgs():
+
+    return list_of_roles
+
+
+def get_orgs(x=0):
     """
         Explanation: Method defined to get all accounts from AWS Organization
-        :params: No params required
-        :return: list of AWS accounts from organization
+        :params: Params required 0 to get orgs details
+        :return: Dict of AWS accounts from organization
     """
 
     client = boto3.client('organizations')
-    response = client.list_accounts()
-    list = []
-    for a in response['Accounts']:
-        list.append(a['Id'])
 
-    return list
+    if x == 0:
+        response_desc = client.describe_organization()
+        return response_desc
+    else:
+        response = client.list_accounts()
+        dic_item = dict()
+        for a in response['Accounts']:
+            dic_item[a['Name']] = a['Id']
+
+        return dic_item
 
 
 def send_msg(accounts_from_roles):
     """
         Explanation: Method defined to send msg with report
         :params: list
-        :return: Date of msg sent
+        :return: Date of the message sent
     """
 
     client = boto3.client('sns')
@@ -74,13 +82,55 @@ def send_msg(accounts_from_roles):
         print('msg sent at {}'.format(response['ResponseMetadata']['HTTPHeaders']['date']))
 
 
+def get_session(account_name, account_id):
+    """
+        Explanation: Method defined to get credentials to access other accounts
+        :params: str Account_name
+        :return: dict with new session data
+    """
+    rolearn = '{}{}{}'.format('arn:aws:iam::', account_id, ':role/OrganizationAccountAccessRole')
+    client = boto3.client('sts')
+    response = client.assume_role(
+        RoleArn=rolearn,
+        RoleSessionName=account_name,
+        DurationSeconds=3600)
+
+    return response
+
+
 def lambda_handler(event, context):
 
-    accounts_from_roles = get_roles()
-    send_msg(accounts_from_roles)
+    list_of_roles_from_accounts = list()
+    report_error = list()
+    org_details = get_orgs(0)
+    orgs_from_accounts = get_orgs(1)
+
+    for account_name, account_id in orgs_from_accounts.items():
+        try:
+            session = get_session(account_name, account_id)
+            for roles_from_accounts in get_roles(session):
+                for account in roles_from_accounts['AssumeRolePolicyDocument']['Statement']:
+                    if org_details['Organization']['MasterAccountId'] != account['Principal']['AWS'][13:25]:
+                        if account_id not in account['Principal']['AWS'][13:25]:
+                            list_of_roles_from_accounts.append(roles_from_accounts)
+                            #print(account['Principal']['AWS'][13:25])
+
+        except:
+            #report_error.append(('Error: Without permission to assume role at AWS Account: {} Id : {}'.format(account_name,account_id)))
+            error = dict()
+            error['access_error_account_name'] = account_name
+            error['access_error_account_id'] = account_id
+            report_error.append(error)
+
+    #todo building msg
+    for a in list_of_roles_from_accounts:
+        print(a)
+    #send_msg(report)
+
+
     return {
         'statusCode': 200,
         'body': json.dumps('')
     }
 
-
+lambda_handler('a','b')
